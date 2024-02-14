7f4112bf37fbefd41b06a7b7e42c852101bd3b0d
Alex Hoffmann <hoffalex@shernet.sheridancollege.ca>
2024-02-14 15:55:17 -0500

    Wrk in progress

---
+++ Assets/Week 6/Scripts/SceneName.cs
@@ -0,0 +1,22 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    TextMesh
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
